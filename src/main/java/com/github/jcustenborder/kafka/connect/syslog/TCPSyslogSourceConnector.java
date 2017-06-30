9ece149d99049a75c48c04a66b091635ae42bf21
Jeremy Custenborder <jcustenborder@gmail.com>
2017-06-29 20:17:15 -0500

    Added note about port bindings.

--- src/main/java/com/github/jcustenborder/kafka/connect/syslog/TCPSyslogSourceConnector.java
+++ src/main/java/com/github/jcustenborder/kafka/connect/syslog/TCPSyslogSourceConnector.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,10 +16,14 @@
 package com.github.jcustenborder.kafka.connect.syslog;
 
 import com.github.jcustenborder.kafka.connect.utils.config.Description;
+import com.github.jcustenborder.kafka.connect.utils.config.DocumentationImportant;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.Task;
 
 @Description("Connector is used to receive syslog messages over TCP.")
+@DocumentationImportant("This connector listens on a network port. Running more than one task or running in distributed " +
+    "mode can cause some undesired effects if another task already has the port open. It is recommended that you run this " +
+    "connector in standalone mode.")
 public class TCPSyslogSourceConnector extends SyslogSourceConnector {
   @Override
   public Class<? extends Task> taskClass() {
